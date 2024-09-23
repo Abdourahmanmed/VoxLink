f60e2dd0510598d582a3ed3ea6f4c2a929bb448b
departement informatique de la poste <leonelneymar36@gmail.com>
2024-09-23 16:43:31 +0300

    je suis enrain de commencer les partier agent commercial en commence par le demande de livraison

--- src/components/AgentCommerce/ContentCommerce.tsx
+++ src/components/AgentCommerce/ContentCommerce.tsx
@@ -8,89 +8,21 @@ import { usePathname } from "next/navigation";
 import { DataTableExportattion } from "./DataTableExportation";
 import { DemandeColumns, DemandeLivraison } from "./livreColumns";
 import { DataTableLivraison } from "./DataTableLivraison";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { useForm } from "react-hook-form";
+import { SelectionCompagne } from "@/Schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 
 export default function ContentCommerce() {
     const [fileName, setFileName] = useState('');
     const path = usePathname();
+    const [DemandeLivraison, setDemandeLivraison] = useState<DemandeLivraison[]>([])
+    const [ListeContact, setListeContact] = useState<User[]>([]);
 
-    const Data: User[] = [  // Correction ici : data est maintenant un tableau d'objets User
-        {
-            id: '1',
-            Nom: "Donald Trump",
-            Telephone: 77323110
-        },
-        {
-            id: '2',
-            Nom: "fatouma ali",
-            Telephone: 77323110
-        },
-        {
-            id: '3',
-            Nom: "teon hernandez",
-            Telephone: 77323110
-        },
-        {
-            id: '4',
-            Nom: "Donald Trump",
-            Telephone: 77323110
-        },
-        {
-            id: '5',
-            Nom: "fatouma ali",
-            Telephone: 77323110
-        },
-        {
-            id: '6',
-            Nom: "teon hernandez",
-            Telephone: 77323110
-        },
-        {
-            id: '7',
-            Nom: "Donald Trump",
-            Telephone: 77323110
-        },
-        {
-            id: '8',
-            Nom: "fatouma ali",
-            Telephone: 77323110
-        },
-        {
-            id: '9',
-            Nom: "teon hernandez",
-            Telephone: 77323110
-        },
-        {
-            id: '10',
-            Nom: "teon hernandez",
-            Telephone: 77323110
-        }
-    ];
-    const Datas: DemandeLivraison[] = [  // Correction ici : data est maintenant un tableau d'objets User
-        {
-            id: "10",
-            Nom: "abdi",
-            Telephone: 77112110,
-            adresse: "quartier 7",
-            date: "27-01-2020"
-        },
-        {
-            id: "11",
-            Nom: "Ralia",
-            Telephone: 77112110,
-            adresse: "quartier 7",
-            date: "27-01-2020"
-        },
-        {
-            id: "12",
-            Nom: "Halima",
-            Telephone: 77112110,
-            adresse: "quartier 7",
-            date: "27-01-2020"
-        },
-
-    ];
-
+  
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if (file) {
@@ -99,7 +31,34 @@ export default function ContentCommerce() {
             setFileName('Aucun fichier sélectionné');
         }
     };
+    // controle de la selection du compagne
+    const selcte = useForm<z.infer<typeof SelectionCompagne>>({
+        resolver: zodResolver(SelectionCompagne),
+        defaultValues: {
+            Compagne: ""
+        },
+    })
 
+    // Fonction pour gérer les changements d'état et récupérer les contacts à rappeler
+    const handleChange = async (value: z.infer<typeof SelectionCompagne>) => {
+        console.log(value);
+    };
+
+    //fonction pour gerer
+    const fetchDemandeLivraison = async () =>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+    const fetchContactQualifier = async () =>{
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
 
     return (
         <main>
@@ -107,13 +66,37 @@ export default function ContentCommerce() {
                 <div className="h-max p-4 text-blue">
                     <form className="bg-blanc w-full h-max rounded-lg shadow-blue p-2 flex gap-2 flex-col">
                         <div className="w-full">
-                            <select name="campagne" id="campagne" className="relative outline-none border border-blue rounded-lg px-4 w-max py-1 cursor-pointer">
-                                <option value="Recouvrement">Recouvrement</option>
-                                <option value="E-suuq">E-suuq</option>
-                                <option value="Colis-Ems">Colis EMS</option>
-                                <option value="petit_paquet">Petit Paquet</option>
-                                <option value="Amazone">Amazone</option>
-                            </select>
+                            <Form {...selcte}>
+                                <FormField
+                                    control={selcte.control}
+                                    name="Compagne"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            {/* <FormLabel className="text-blue">Compagne</FormLabel> */}
+                                            <FormControl>
+                                                <Select
+                                                    {...field}
+                                                    onValueChange={(value) => {
+                                                        field.onChange(value);  // Mettre à jour la valeur du formulaire
+                                                        // handleChange(value);    // Effectuer des actions supplémentaires
+                                                    }}
+                                                >
+                                                    <SelectTrigger className="shadow border border-blue rounded-[10px] w-full py-2 px-3 text-blue focus:outline-none placeholder-blue/70 caret-blue w-max">
+                                                        <SelectValue placeholder="Sélectionner une compagne" />
+                                                    </SelectTrigger>
+                                                    <SelectContent>
+                                                        <SelectItem value="E-suuq">E-suuq</SelectItem>
+                                                        <SelectItem value="Recouvrement">Recouvrement</SelectItem>
+                                                        <SelectItem value="Petite paquet">Petite paquet</SelectItem>
+                                                        <SelectItem value="Colis-Ems">Colis Ems</SelectItem>
+                                                    </SelectContent>
+                                                </Select>
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </Form>
                         </div>
                         <div className="w-full flex justify-center">
                             {/* Bouton d'importation avec icône */}
@@ -140,27 +123,52 @@ export default function ContentCommerce() {
 
                     </form>
                     <h1 className="text-md font-medium py-2">Liste de contact appeller et leur qualifcation</h1>
-                    <DataTableImportation data={Data} columns={Columns} typeName="Nom" />
+                    <DataTableImportation data={ListeContact} columns={Columns} typeName="Nom" FetchData={handleChange} />
                 </div>
             )}
             {path && path === "/Commercial/Exportation" && (
                 <div className="h-max p-4 text-blue">
                     <div className="bg-blanc w-full h-max rounded-lg shadow-blue p-6 flex">
-                        <select name="campagne" id="campagne" className="relative outline-none border border-blue rounded-lg px-4 w-full py-2 cursor-pointer">
-                            <option value="Recouvrement">Recouvrement</option>
-                            <option value="E-suuq">E-suuq</option>
-                            <option value="Colis-Ems">Colis EMS</option>
-                            <option value="petit_paquet">Petit Paquet</option>
-                        </select>
+                        <Form {...selcte}>
+                            <FormField
+                                control={selcte.control}
+                                name="Compagne"
+                                render={({ field }) => (
+                                    <FormItem className="w-full">
+                                        {/* <FormLabel className="text-blue">Compagne</FormLabel> */}
+                                        <FormControl >
+                                            <Select
+                                                {...field}
+                                                onValueChange={(value) => {
+                                                    field.onChange(value);  // Mettre à jour la valeur du formulaire
+                                                    // handleChange(value);    // Effectuer des actions supplémentaires
+                                                }}
+                                            >
+                                                <SelectTrigger className="shadow border border-blue rounded-[10px] w-full py-2 px-3 text-blue focus:outline-none placeholder-blue/70 caret-blue">
+                                                    <SelectValue placeholder="Sélectionner une compagne" />
+                                                </SelectTrigger>
+                                                <SelectContent className="w-full">
+                                                    <SelectItem value="E-suuq">E-suuq</SelectItem>
+                                                    <SelectItem value="Recouvrement">Recouvrement</SelectItem>
+                                                    <SelectItem value="Petite paquet">Petite paquet</SelectItem>
+                                                    <SelectItem value="Colis-Ems">Colis Ems</SelectItem>
+                                                </SelectContent>
+                                            </Select>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </Form>
                     </div>
                     <h1 className="text-md font-medium py-2">Liste de contact appeller et leur qualifcation</h1>
-                    <DataTableExportattion data={Data} columns={Columns} typeName="Nom" />
+                    <DataTableExportattion data={ListeContact} columns={Columns} typeName="Nom" />
                 </div>
             )}
             {path && path === "/Commercial/Demande_livraison" && (
                 <div className="h-max p-4 text-blue">
                     <h1 className="text-md font-medium py-2">Liste de demande livraison</h1>
-                    <DataTableLivraison data={Datas} columns={DemandeColumns} typeName="Nom" />
+                    <DataTableLivraison data={DemandeLivraison} columns={DemandeColumns} typeName="Nom" />
                 </div>
             )}
 
